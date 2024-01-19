b6b67f04539c678a725620d40dd99f680971fdf7
Ivan <proffesor.09@mail.ru>
2024-01-19 21:51:36 +0300

    исправлены недоработки.

--- shoping/urls.py
+++ shoping/urls.py
@@ -1,10 +1,13 @@
-from itertools import product
-
 from django.urls import path
+from shoping.views import index, product, product_information
+from . import views
 
-from shoping.views import index
+app_name = 'shoping'  # Указываем пространство имен для приложения "shoping"
 
 urlpatterns = [
     path('', index, name="index"),
+    path('<int:pk>/product/', product, name="product"),
+    path('<int:pk>/product_information/', product_information, name="product_information"),
+
 
 ]
--- shoping/views.py
+++ shoping/views.py
@@ -6,10 +6,27 @@ from shoping.models import Category, Product
 # Create your views here.
 def index(request):
     context = {
-        "object_list": Product.objects.all(),
+        "object_list": Category.objects.all(),
         "title": "мясо свежее"
     }
     return render(request, 'shoping/index.html', context)
 
+def product(request, pk):
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        "object_list": Product.objects.filter(category_id=pk),
+        "title": f"{category_item.name}"
+    }
+    return render(request, 'shoping/product.html', context)
 
-#
+
+
+def product_information(request, pk):
+    # Получить объект товара по его идентификатору
+    product = Product.objects.get(pk=pk)
+
+    context = {
+        'object': product,  # Передача объекта товара в контекст шаблона
+    }
+
+    return render(request, 'shoping/product_information.html', context)
